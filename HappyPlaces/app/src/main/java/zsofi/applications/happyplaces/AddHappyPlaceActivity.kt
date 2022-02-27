fba5940f8d3c9ba63d204859fa84e2d2d1b19160
zsofia_mandi <mandi.zsofia@gmail.com>
2022-02-27 14:22:16 +0100

    Storing the images on the device

--- HappyPlaces/app/src/main/java/zsofi/applications/happyplaces/AddHappyPlaceActivity.kt
+++ HappyPlaces/app/src/main/java/zsofi/applications/happyplaces/AddHappyPlaceActivity.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -11,11 +13,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.drawable.toBitmap
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -23,7 +27,10 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
 import zsofi.applications.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.util.*
 
 class AddHappyPlaceActivity : AppCompatActivity(),View.OnClickListener {
@@ -38,6 +45,10 @@ class AddHappyPlaceActivity : AppCompatActivity(),View.OnClickListener {
                 try {
                     binding?.ivImageUpload?.setPadding(0,0,0,0)
                     binding?.ivImageUpload?.setImageURI(result.data?.data)
+                    val drawable = binding?.ivImageUpload?.drawable
+                    val bitmap = drawable!!.toBitmap()
+                    val saveImageToInternalStorage = saveImageToInternalStorage(bitmap)
+                    Log.e("Saved image: ", "Path :: $saveImageToInternalStorage")
                     // TODO to check why did the border from the image disappear
                 }catch (e: IOException){
                     e.printStackTrace()
@@ -55,6 +66,8 @@ class AddHappyPlaceActivity : AppCompatActivity(),View.OnClickListener {
                 val thumbNail : Bitmap = result.data?.extras!!.get("data") as Bitmap
                 binding?.ivImageUpload?.setPadding(0,0,0,0)
                 binding?.ivImageUpload?.setImageBitmap(thumbNail)
+                val saveImageToInternalStorage = saveImageToInternalStorage(thumbNail)
+                Log.e("Saved image: ", "Path :: $saveImageToInternalStorage")
             }
         }
 
@@ -195,6 +208,27 @@ class AddHappyPlaceActivity : AppCompatActivity(),View.OnClickListener {
             }.setNegativeButton("Cancel"){dialog, _ ->
                 dialog.dismiss()
             }.show()
+    }
+
+    private fun saveImageToInternalStorage(bitmap: Bitmap): Uri{
+        val wrapper = ContextWrapper(applicationContext)
+        // Mode private - Other apps wont be able to access this directory
+        var file = wrapper.getDir(IMAGE_DIRECTORY,Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg") // file, filename
+
+        try{
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+        return Uri.parse(file.absolutePath)
+    }
+
+    companion object{
+        private const val IMAGE_DIRECTORY = "HappyPlacesImages"
     }
 
     override fun onDestroy() {
